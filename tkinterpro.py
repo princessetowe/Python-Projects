ce1d5310b06db863965820189c3241d9ec46c096
Princess Etowe <106072527+princessetowe@users.noreply.github.com>
2024-06-06 11:01:07 +0100

    Delete tkinterpro.py

--- tkinterpro.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import *
-root = tk.Tk()
-root.title("App")
-root.geometry("400x400")
-label = tk.Label(root, text="First Tkinter!", font=(16))
-#label.pack(pady=20)
-label.grid()
-root.mainloop()
